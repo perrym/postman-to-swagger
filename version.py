3e375a9cc2d80a125e64c55dc7f6dfec9ca66e7e
perrym <67121078+perrym@users.noreply.github.com>
2025-11-23 14:22:02 +0100

    Update version.py

--- version.py
+++ version.py
@@ -1,10 +1,6 @@
 # 
-# Licensed under the MIT License. 
+# Licensed under the AGPL-v3.0 License. 
 # Copyright (c) 2025 Perry Mertens
 #
 # See the LICENSE file for full license text.
-<<<<<<< HEAD
-__version__ = "1.0.0"
-=======
-__version__ = "2.0.0-Release"
->>>>>>> e251021d3aa3bb4ce031599fb682af264ea80e5b
+__version__ = "1.0.0-beta"
